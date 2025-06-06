62c52a1fc3a68e57d1823cf2b2a896a0da2cf747
glabrie <ghil@hey.com>
2025-06-06 16:17:45 -0400

    started my nvf config

--- modules/nvf/nvf-configuration.nix
+++ modules/nvf/nvf-configuration.nix
@@ -4,24 +4,41 @@
   programs.nvf = {
     enable = true;
     settings = {
-      vim.theme = {
-        enable = true;
-        name = "gruvbox";
-        style = "dark";
-      };
+      vim = {
+        theme = {
+          enable = true;
+          name = "gruvbox";
+          style = "dark";
+        };
 
-      vim.statusline.lualine.enable = true;
-      vim.telescope.enable = true;
-      vim.autocomplete.nvim-cmp.enable = true;
+      # Settings and plugins
+        globals.mapleader = " ";
+        viAlias = false;
+        vimAlias = true;
+        statusline.lualine.enable = true;
+        telescope.enable = true;
+        autocomplete.nvim-cmp.enable = true;
+        languages = {
+          lsp.enable = true;
+          enableTreesitter = true;
+          # Languages 
+          nix.enable = true;
+          python.enable = true;
+          go.enable = true;
+          rust.enable = true;
+        clipboard = {
+          enable = true;
+          registers = "unnamedplus";
+        };
 
-      vim.languages = {
-        enableLSP = true;
-        enableTreesitter = true;
-      
-        nix.enable = true;
-        python.enable = true;
-        go.enable = true;
-        rust.enable = true;
+      # Options
+        options = {
+          tabstop = 2;
+          softtabstop = 2;
+          shiftwidth = 2;
+        };
+      # Binds
+        }; 
       };
     };
   };
