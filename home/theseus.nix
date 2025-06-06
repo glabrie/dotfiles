59d3d103bbf130d7a600684e9a7c227e127c591b
glabrie <ghil@hey.com>
2025-06-06 13:03:19 -0400

    initial commit

---
+++ home/theseus.nix
@@ -0,0 +1,59 @@
+# core home manager config for theseus
+{ config, pkgs, inputs, ... }:
+{
+
+  # Settings
+  home.username = "ghil";
+  home.homeDirectory = "/home/ghil";
+  home.stateVersion = "25.05";
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  # Package management
+  home.packages = with pkgs; [
+    # Apps
+    atuin
+    bottom
+    brave
+    cava
+    cliphist
+    discord
+    element-desktop
+    gh
+    ghostty
+    ncspot
+    obsidian
+    ranger
+    slack
+    tana
+    telegram-desktop
+  ];
+
+  programs.home-manager.enable = true;
+
+  programs.neovim.extraLuaPackages = true;
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      boot-garbage = "sudo /run/current-system/bin/switch-to-configuration boot";
+      collect-garbage = "sudo nix-collect-garbage -d";
+      dots ="cd ~/.dotfiles/";
+      flup = "nix flake update";
+      ga = "git add *";
+      gc = "git commit --verbose";
+      gp = "git push";
+      ll = "ls -l";
+      v = "nvim";
+      update = "sudo nixos-rebuild switch --flake ~/.dotfiles/#default";
+    };
+  };
+}
