544b19cd6c9b56746205b428fc8ca207c72fa832
glabrie <ghil@hey.com>
2025-06-07 20:29:51 -0400

    small change to opacity

--- home/kitty.nix
+++ home/kitty.nix
@@ -12,7 +12,7 @@
       confirm_os_window_close = 0;
       dynamic_background_opacity = true;
       enable_audio_bell = false;
-      background_opacity = "0.5";
+      background_opacity = "0.8";
       background_blur = 5;
       cursor_trail = 8;
     };
