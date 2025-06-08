4829e17a130aabd6b77f01cdd84bf5cf435ace5c
glabrie <ghil@hey.com>
2025-06-07 20:22:39 -0400

    fixed typo

--- modules/theseus/configuration.nix
+++ modules/theseus/configuration.nix
@@ -106,7 +106,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  font-awesome-5
+  font-awesome_5
   fzf
   gcc
   git
