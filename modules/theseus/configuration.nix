2fcc3faf454d5c90f04b6631974117476a1feeb1
glabrie <ghil@hey.com>
2025-06-07 20:49:28 -0400

    added font awesome

--- modules/theseus/configuration.nix
+++ modules/theseus/configuration.nix
@@ -106,6 +106,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+  font-awesome
   font-awesome_5
   fzf
   gcc
