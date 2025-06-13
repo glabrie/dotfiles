5ebaf31b6dbbbd9bb724dd7826ded99125f742fb
glabrie <ghil@hey.com>
2025-06-12 20:22:56 -0400

    update and install steam

--- home/theseus.nix
+++ home/theseus.nix
@@ -76,6 +76,12 @@
     };
   };
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
   programs.zsh = {
     enable = true;
     shellAliases = {
