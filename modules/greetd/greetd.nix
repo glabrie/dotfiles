fc35524b50d2920c28aeebfa8d28de2552d35089
Guillaume Labrie <glabrie85@gmail.com>
2026-01-20 18:02:47 -0500

    hyprland complains again

--- modules/greetd/greetd.nix
+++ modules/greetd/greetd.nix
@@ -7,7 +7,7 @@ in
     enable = true;
     settings = {
       default_session = {
-        command = "${tuigreet} --time --remember --cmd Hyprland";
+        command = "${tuigreet} --time --remember --cmd start-Hyprland";
         user = "greeter";
       };
     };
