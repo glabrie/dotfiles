9bb445b7efcf0fa958e620cfeaf2c597edadb117
Guillaume Labrie <ghil@hey.com>
2026-01-31 18:14:06 -0500

    NixOS 2026-01-31T23:13:53+00:00 Build theseus 14

---
+++ rebuild.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+cd $(dirname "$0") &&
+
+printf "== Adding files to git... \n" &&
+git add . &&
+
+printf "== Building NixOS... \n" &&
+sudo nixos-rebuild switch --flake .#$(hostname) &&
+
+printf "== Commiting to git... \n" &&
+DEFAULT_COMMIT_MSG="NixOS $(date -u --iso-8601=seconds) Build $(hostname) $(readlink /nix/var/nix/profiles/system | cut -d- -f2)" &&
+read -ep "Commit message ($DEFAULT_COMMIT_MSG): " PROMPT_COMMIT_MSG &&
+COMMIT_MSG=$([[ -z "${PROMPT_COMMIT_MSG// /}" ]] && printf "$DEFAULT_COMMIT_MSG" || printf "$PROMPT_COMMIT_MSG\n$DEFAULT_COMMIT_MSG")
+git commit -m "$COMMIT_MSG" && 
+
+printf "== Pushing to remote...\n" &&
+git push
