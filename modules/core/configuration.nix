a58e9934a4d13dbbd3b266e05f164b0bf7316716
Guillaume Labrie <ghil@hey.com>
2026-02-10 22:47:57 -0500

    new kernel NixOS 2026-02-11T03:46:40+00:00 Build theseus 36

--- modules/core/configuration.nix
+++ modules/core/configuration.nix
@@ -13,6 +13,7 @@
 
   # Bootloader.
   boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
     extraModulePackages = with config.boot.kernelPackages; [ xpadneo ];
     extraModprobeConfig = ''
     options bluetooth disable_ertm=Y
