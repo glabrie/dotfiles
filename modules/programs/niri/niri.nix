f5799f428a8a92fe167d6c15c6ea915c3434879b
Guillaume Labrie <ghil@hey.com>
2026-04-04 12:37:56 -0400

    added portal-gnome NixOS 2026-04-04T16:37:37+00:00 Build theseus 94

--- modules/programs/niri/niri.nix
+++ modules/programs/niri/niri.nix
@@ -8,7 +8,13 @@
 
     xdg.portal = {
       enable = true;
-      extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+      extraPortals = [ 
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-gnome
+      ];
+      config = {
+        common.default = [ "gnome" ];
+      };
     };
 
     environment.systemPackages = [ pkgs.xwayland-satellite ];
