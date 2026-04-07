2110e71d0a697a710b392456241208862160d55a
Guillaume Labrie <ghil@hey.com>
2026-04-07 13:23:18 -0400

    revert to zsh for now NixOS 2026-04-07T17:23:10+00:00 Build theseus 110

--- modules/users/ghil/ghil.nix
+++ modules/users/ghil/ghil.nix
@@ -11,7 +11,7 @@ in
       isNormalUser = true;
       description = "Guillaume Labrie";
       extraGroups = [ "networkmanager" "wheel" ];
-      shell = pkgs.nushell;
+      shell = pkgs.zsh;
     };
 
     home-manager.users."${username}" = {
