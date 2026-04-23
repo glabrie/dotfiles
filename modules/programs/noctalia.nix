50c0c37716e01b88b95dfd2ddf7b67633446e448
Guillaume Labrie <ghil@hey.com>
2026-04-23 11:11:43 -0400

    updated rebuild.nu NixOS 2026-04-23T11:11:34-0400 Build theseus 160

--- modules/programs/noctalia.nix
+++ modules/programs/noctalia.nix
@@ -1,93 +1,97 @@
 { inputs, ... }:
 {
-  flake.modules.homeManager.noctalia = 
-  { ... }: 
+  flake.modules.homeManager.noctalia =
+  { ... }:
   {
     imports = [ inputs.noctalia.homeModules.default ];
-
-    # configure options
     programs.noctalia-shell = {
       enable = true;
       systemd.enable = true;
-      settings = {
-        # Bar options
-        bar = {
-          barType = "framed";
-          density = "default";
-          position = "top";
-          showCapsule = false;
-          backgroundOpacity = 0.8;
-          useSeparateOpacity = true;
-          floating = false;
-          marginVertical = 4;
-          marginHorizontal = 4;
-          frameThickness = 8;
-          frameRadius = 12;
-          outerCorners = true;
-          exclusive = true;
-          widgets = {
-            left = [
-              {
-                id = "ControlCenter";
-                useDistroLogo = true;
-              }
-            ];
-            center = [
-              {
-                hideUnoccupied = false;
-                id = "Workspace";
-                labelMode = "none";
-              }
-            ];
-            right = [
-              {
-                alwaysShowPercentage = false;
-                id = "Battery";
-                warningThreshold = 30;
-              }
-              {
-                id = "Network";
-              }
-              {
-                id = "Bluetooth";
-              }
-              {
-                formatHorizontal = "HH:mm";
-                formatVertical = "HH mm";
-                id = "Clock";
-                useMonospacedFont = true;
-                usePrimaryColor = true;
-              }
-            ];
-          };
-        };
-        colorSchemes = {
-          predefinedScheme = "Tokyo-Night";
-          darkMode = true;
-        };
-        general = {
-          avatarImage = "/home/ghil/.face";
-          radiusRatio = 0.2;
-          dimmerOpacity = 0.2;
-          showScreenCorners = true;
-          forceBlackScreenCorners = true;
-          enableShadows = true;
-          telemetryEnabled = false;
-          autoStartAuth = false;
-        };
-        ui = {
-          fontDefault = "Maple Mono";
-          fontFixed = "Maple Mono";
-          panelBackgroundOpacity = 0.93;
-          boxBorderEnabled = false;
-        };
-        location = {
-          monthBeforeDay = false;
-          name = "Quebec, Canada";
-          useFahrenheit = false;
-          firstDayOfWeek = -1;
-        };
-        wallpaper = {
+
+settings = {
+  bar = {
+    barType = "framed";
+    density = "default";
+    position = "top";
+    showCapsule = false;
+    backgroundOpacity = 0.9;
+    useSeparateOpacity = true;
+    floating = false;
+    marginVertical = 4;
+    marginHorizontal = 4;
+    frameThickness = 8;
+    frameRadius = 12;
+    outerCorners = true;
+    exclusive = true;
+
+widgets = {
+    left = [
+      {
+        id = "ControlCenter";
+        useDistroLogo = true;
+      }
+    ];
+    center = [
+      {
+        hideUnoccupied = false;
+        id = "Workspace";
+        labelMode = "none";
+      }
+    ];
+    right = [
+      {
+        alwaysShowPercentage = false;
+        id = "Battery";
+        warningThreshold = 30;
+      }
+      {
+        id = "Network";
+      }
+      {
+        id = "Bluetooth";
+      }
+      {
+        formatHorizontal = "HH:mm";
+        formatVertical = "HH mm";
+        id = "Clock";
+        useMonospacedFont = true;
+        usePrimaryColor = true;
+      }
+    ];
+  };
+};
+
+colorSchemes = {
+  predefinedScheme = "Tokyo-Night";
+  darkMode = true;
+};
+
+general = {
+  avatarImage = "/home/ghil/.face";
+  radiusRatio = 0.2;
+  dimmerOpacity = 0.2;
+  showScreenCorners = true;
+  forceBlackScreenCorners = true;
+  enableShadows = true;
+  telemetryEnabled = false;
+  autoStartAuth = false;
+};
+
+ui = {
+  fontDefault = "Maple Mono";
+  fontFixed = "Maple Mono";
+  panelBackgroundOpacity = 0.93;
+  boxBorderEnabled = false;
+};
+
+location = {
+  monthBeforeDay = false;
+  name = "Quebec, Canada";
+  useFahrenheit = false;
+  firstDayOfWeek = -1;
+};
+
+wallpaper = {
           enabled = true;
           directory = "$HOME/images/walls";
         };
@@ -96,7 +100,7 @@
         };
 
       };
-      
+
     };
   };
 }
--- rebuild.nu
+++ rebuild.nu
@@ -1,21 +1,31 @@
 #!/usr/bin/env nu
 
+mut timer = []
+
 cd ~/.dotfiles
 
 print "/---- Adding files to git ----/ \n"
-git add .
+$timer ++= [{step: "git add", time: (timeit { git add . })}]
 
 print "/---- Tangling org files ----/ \n"
-glob **/*.org | where ($it | path basename) != "README.org" | each { |f| let eval_str = '(org-babel-tangle-file "' + ($f | str trim) + '")'; emacs --batch -l org --eval $eval_str }
+$timer ++= [{step: "tangle org files", time: (timeit {
+       glob **/*.org | where ($it | path basename) != "README.org" | each { |f| let eval_str = '(org-babel-tangle-file "' + ($f | str trim) + '")'
+       emacs --batch -l org --eval $eval_str
+       }
+})}]
 
 print "/---- Rebuilding NixOS ----/ \n"
-sudo nixos-rebuild switch --flake $".#(hostname)"
+$timer ++= [{step:"NixOS rebuild", time: (timeit { sudo nixos-rebuild switch --flake $".#(hostname)" })}]
 
 print "/---- Committing to git ----/ \n"
 let default_msg = $"NixOS (date now | format date '%Y-%m-%dT%H:%M:%S%z') Build (hostname) (readlink /nix/var/nix/profiles/system | path basename | split row '-' | get 1)"
 let user_msg = (input $"Commit message \(($default_msg)\): ")
 let commit_msg = if ($user_msg | str trim | is-empty) { $default_msg } else { $"($user_msg)\n($default_msg)" }
-git commit -m $commit_msg
+$timer ++= [{step: "Git commit", time: (timeit { git commit -m $commit_msg })}]
 
 print "/---- Pushing to remote ----/ \n"
-git push
+$timer ++= [{step: "Git push", time: (timeit { git push })}]
+
+print "/---- Summary ----/"
+$timer | table
+print $"System rebuilt in: ($timer | get time | math sum)"
