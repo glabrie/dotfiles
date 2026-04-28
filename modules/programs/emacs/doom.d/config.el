cb139b82408a55350e116a760c096b7c8aff71cd
Guillaume Labrie <ghil@hey.com>
2026-04-28 18:29:32 -0400

    more blur NixOS 2026-04-28T18:29:27-0400 Build theseus 175

--- modules/programs/emacs/doom.d/config.el
+++ modules/programs/emacs/doom.d/config.el
@@ -21,8 +21,8 @@
 (global-display-line-numbers-mode +1)                                   ;; But you can't take the vim from the man
 
 ;; Transparent background so niri's blur shows through
-(add-to-list 'default-frame-alist '(alpha-background . 85))
-(set-frame-parameter nil 'alpha-background 85)
+(add-to-list 'default-frame-alist '(alpha-background . 90))
+(set-frame-parameter nil 'alpha-background 90)
 
 (use-package! org-auto-tangle                                           ;; Auto-tangle, to tangle auto-magically!
   :hook (org-mode . org-auto-tangle-mode))
