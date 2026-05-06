748a831a32b5dea95c7826942edd293959cfd5aa
Guillaume Labrie <ghil@hey.com>
2026-05-06 16:03:34 -0400

    update, dropped hypr, migration to literate progress NixOS 2026-05-06T15:40:02-0400 Build theseus 237

--- modules/nix/home-manager.nix
+++ modules/nix/home-manager.nix
@@ -1,29 +1,23 @@
 { inputs, ... }:
-let
-  home-manager-config =
-    { lib, ... }:
-    {
-      home-manager = {
-        verbose = true;
-        useUserPackages = true;
-        useGlobalPkgs = true;
-        backupFileExtension = "backup";
-        backupCommand = "rm";
-        overwriteBackup = true;
-      };
-    };
-in
-  {
-    imports = 
-    [ 
-      inputs.flake-parts.flakeModules.modules
-      inputs.home-manager.flakeModules.home-manager 
-    ];
+{
+  imports = [
+    inputs.flake-parts.flakeModules.modules
+    inputs.home-manager.flakeModules.home-manager
+  ];
 
-    flake.modules.nixos.home-manager = {
-      imports = [
-        inputs.home-manager.nixosModules.home-manager
-        home-manager-config
-      ];
-    };
+  flake.modules.nixos.home-manager = {
+    imports = [
+      inputs.home-manager.nixosModules.home-manager
+      {
+        home-manager = {
+            verbose = true;
+            useUserPackages = true;
+            useGlobalPkgs = true;
+            backupFileExtension = "backup";
+            backupCommand = "rm";
+            overwriteBackup = true;
+        };
+      }
+    ];
+  };
 }
