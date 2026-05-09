e753c436f3a8dc91f4920dfc1fb3768190870d25
Guillaume Labrie <ghil@hey.com>
2026-05-09 14:35:48 -0400

    added rar support to 7z NixOS 2026-05-09T14:35:12-0400 Build theseus 256

--- modules/system/settings/gaming.nix
+++ modules/system/settings/gaming.nix
@@ -10,8 +10,21 @@
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     };
+
     # Enable xbox controller support
     hardware.xpadneo.enable = true;
     hardware.xone.enable = true;
+  };
+
+  flake.modules.homeManager.gaming =
+  { ... }:
+  {
+    xdg.desktopEntries.steam = {
+      name = "Steam";
+      exec = "DRI_PRIME=1 steam %U";
+      icon = "steam";
+      terminal = false;
+      categories = [ "Network" "FileTransfer" "Game" ];
+    };
   };
 }
