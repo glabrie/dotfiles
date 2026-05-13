c1a2341cf4cca0e42e24deec51cc453bdb50ca6a
Guillaume Labrie <ghil@hey.com>
2026-05-12 23:17:15 -0400

    fix for tailscale stealing DNS NixOS 2026-05-12T23:16:25-0400 Build theseus 263

--- modules/system/settings/networking.nix
+++ modules/system/settings/networking.nix
@@ -2,10 +2,20 @@
 { inputs, ... }:
 {
   flake.modules.nixos.networking =
-  { ... }:
+  { pkgs, ... }:
   {
     networking.networkmanager.enable = true;
     services.tailscale.enable = true;
+
+    systemd.services.tailscale-resume = {
+      description = "Restart tailscaled after resume";
+      wantedBy = [ "post-resume.target" ];
+      after = [ "post-resume.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.systemd}/bin/systemctl restart tailscaled.service";
+      };
+    };
   };
 
   flake.modules.homeManager.networking =
