2609e15a40be0279d4722c7fe96c87a21839cd8b
Guillaume Labrie <ghil@hey.com>
2026-05-14 21:58:58 -0400

    again NixOS 2026-05-14T21:58:20-0400 Build theseus 272

--- modules/programs/tmux.nix
+++ modules/programs/tmux.nix
@@ -14,9 +14,13 @@
         set -g status-right-length 100
         set -g status-left-length 100
         set -g automatic-rename on
-        set -g automatic-rename-format "#{pane_current_command}"
+        set -g allow-rename off
+        set -g automatic-rename-format "#{s/nu/nushell/:pane_current_command}"
         set -ga terminal-overrides ",*256col*:Tc"
         set -ga terminal-features ",*256col*:bce"
+        set -g extended-keys on
+        set -g window-style 'bg=default'
+        set -g window-active-style 'bg=default'
 
         set -g @catppuccin_flavor "mocha"
         set -g @catppuccin_window_status_style "rounded"
